dc75f96cd2fd4c93641bc8159cc1c6d0c114ed43
Tijmen de Graaff <tijmen76@gmail.com>
2019-04-16 18:44:22 +0200

    setup scss and file setup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { render } from "react-dom";
+import Header from "./components/header";
+import Text from "./components/text";
+
+import "./styles/styles.scss";
 
 const App = () => (
   <div>
-    <h1>Boilerplate react App</h1>
-    <p>Just checking to see if everything is working</p>
-    <p>Is it?</p>
+    <Header />
+    <Text />
   </div>
 );
 
